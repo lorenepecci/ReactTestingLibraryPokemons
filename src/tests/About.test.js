27039adb1ef82e55279dd8bdd99a4cff7f67ea88
Lorene Pecci <lorenepecci@gmail.com>
2022-02-18 17:01:40 -0300

    req2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,31 @@
-/* test about  */
-test('', () => { });
+/* test about About Pokédex */
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+beforeEach(() => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/about');
+});
+
+describe('testando o About ', () => {
+  test('ter o h2 com o texto About Pokédex ', () => {
+    const aboutPoke = screen.getByRole('heading', { level: 2, name: 'About Pokédex' });
+    expect(aboutPoke).toBeInTheDocument();
+  });
+  test('se contem a imagem `https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png`.', () => {
+    const image = screen.getByAltText('Pokédex');
+    expect(image.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+
+  test('se contem dois paragrafos ', () => {
+    /*  const paragrafos = screen.queryByAllRole('p'); */
+
+    const textPokemon = screen.getByText(/application simulates/i);
+    expect(textPokemon).toBeInTheDocument();
+    const textPokemon2 = screen.getByText(/for each one of them/i);
+    expect(textPokemon).toBeInTheDocument();
+    expect(textPokemon2).toBeInTheDocument();
+  });
+});
